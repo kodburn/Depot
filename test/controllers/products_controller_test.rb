b5dc4711fc1e53c1d5556d81554509beb4b00a9f
kodburn <kodburn@gmail.com>
2016-11-22 22:30:18 +0100

    Add validations to Product model

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,42 +3,47 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+      title: 'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url: 'lorem.jpg',
+      price: 19.95
+    }
   end
 
-  test "should get index" do
+  test 'should get index' do
     get products_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_product_url
     assert_response :success
   end
 
-  test "should create product" do
+  test 'should create product' do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, product: @update
     end
-
     assert_redirected_to product_url(Product.last)
   end
 
-  test "should show product" do
+  test 'should show product' do
     get product_url(@product)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_product_url(@product)
     assert_response :success
   end
 
-  test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+  test 'should update product' do
+    patch product_url(@product), product: @update
     assert_redirected_to product_url(@product)
   end
 
-  test "should destroy product" do
+  test 'should destroy product' do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
     end

17a58be944722aead356a24f674780f39a210e43
kodburn <kodburn@gmail.com>
2016-11-23 00:08:53 +0100

    Add Store controller and view. Update layout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -62,13 +62,14 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list throug.
+  def product_params
+    params.require(:product).permit(:title, :description, :image_url, :price)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end

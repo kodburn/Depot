2b99c70383c887e9929d5d1623243fd07f201b6e
kodburn <kodburn@gmail.com>
2016-11-25 02:24:45 +0100

    Add customer basket

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,25 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i }
+  validates :image_url, allow_blank: true, format: { with: /\.(gif|jpg|png)\Z/i }
 
   def self.latest
     Product.order(:updated_at).last
+  end
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      true
+    else
+      errors.add(:base, 'line items exist')
+      false
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :carts
+  resources :line_items
   root to: 'store#index', as: 'store'
   get 'store/index'
   resources :products

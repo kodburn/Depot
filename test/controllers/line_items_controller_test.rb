6605ff90dfa1a61be7dc823b7bf2b144f6edc3a4
kodburn <kodburn@gmail.com>
2016-12-01 18:36:04 +0100

    Move cart to main layout and add AJAX

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,6 +23,15 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     #assert_redirected_to cart_path(assigns(:line_item).cart)
   end
 
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:ruby).id
+    end
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
+  end
   test 'should show line_item' do
     get line_item_url(@line_item)
     assert_response :success
